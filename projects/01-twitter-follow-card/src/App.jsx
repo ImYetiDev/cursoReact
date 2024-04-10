a141661b9f3dce270fce07accbd4bb4372f0ab0f
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-04-09 20:58:31 -0500

    first commit

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+export function App() {
+  return (
+    <div class=".tw-followCard">
+      <header>
+        <img src="https://unavatar.io/vegeta777" alt="" />
+        <div>
+            <strong>ImYetiDev</strong>
+            <span>@imyetidev</span>
+        </div>
+      </header>
+
+
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </div>
+  );
+}
