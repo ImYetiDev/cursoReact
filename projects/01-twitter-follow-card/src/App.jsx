d7d52e2cfcb69013a57b23b54496d94241a4bda7
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-04-12 23:03:54 -0500

    Update TwitterFollowCard component to use dynamic user data in App.jsx

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -3,19 +3,43 @@ import { TwitterFollowCard } from './TwitterFollowCard';
 
 export function App() {
 
+    const users = [
+        {
+            userName: 'imyetidev',
+            name: 'David Uribe',
+            isFollowing: true
+        },
+        {
+            userName: 'midudev',
+            name: 'Miguel Angel',
+            isFollowing: false
+        },
+        {
+            userName: 'elonmusk',
+            name: 'Elon Musk',
+            isFollowing: false
+        },
+        {
+            userName: 'vegetta777',
+            name: 'Samuel Deluque',
+            isFollowing: true
+        }
+    ]
+
     return (
         <section className='App'>
-            <TwitterFollowCard
-                initialIsFollowing={true}
-                userName="imyetidev">
-                David Uribe
-            </TwitterFollowCard>
-
-            <TwitterFollowCard
-                isFollowing={false}
-                userName="midudev">
-                Miguel Angel
-            </TwitterFollowCard>
+            {
+                users.map(user => {
+                    const { userName, name, isFollowing } = user
+                    return (
+                        <TwitterFollowCard
+                        userName={userName}
+                        initialIsFollowing={isFollowing}>
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
         </section>
     )
 }
