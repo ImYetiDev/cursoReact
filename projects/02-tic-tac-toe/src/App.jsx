dfd9362e3ff27fc8a881f84456670aadd70ec7b3
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-04-14 14:17:41 -0500

    Refactor Tic Tac Toe board rendering and update CSS styles in App.jsx

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -11,12 +11,21 @@ const TURNS = {
 const Square = ({ children, isSelected ,updateBoard, index }) => {
   
   const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div className={className}>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
+
+
+
+
 
 function App() {
   const [board, setBoard] = useState(
@@ -24,6 +33,16 @@ function App() {
   )
 
   const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+  }
 
   return (
     <main className='board'>
@@ -34,8 +53,10 @@ function App() {
             return (
               <Square
                 key={index}
+                index={index}
+                updateBoard={updateBoard}
               >
-
+                {board[index]}
               </Square>
             )
           })
